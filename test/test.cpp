6797e6821e77577fc419977111a3f0e628e2eebd
Simon Walther <simon.walt@hotmail.com>
2019-06-07 11:47:22 +0200

    add a test on customer statement

--- test/test.cpp
+++ test/test.cpp
@@ -3,8 +3,30 @@
 //
 
 #include <iostream>
+#include "Customer.cpp"
 #include "gtest/gtest.h"
 
-TEST(sample_test_case, sample_test) {
-    EXPECT_EQ(1, 1);
+namespace {
+    class CustomerTest : public ::testing::Test {
+    protected:
+        // void SetUp() override {}
+        // void TearDown() override {}
+    };
+
+    TEST(CustomerTest, StatementWorks) {
+        Customer customer("Olivier");
+        customer.addRental( Rental( Movie("Karate Kid"), 7));
+        customer.addRental( Rental( Movie( "Avengers: Endgame", Movie::NEW_RELEASE ), 5));
+        customer.addRental( Rental( Movie("Snow White", Movie::CHILDRENS), 3 ));
+
+        std::string statement = customer.statement();
+
+        EXPECT_EQ(statement, "Rental Record for Olivier\n"
+                             "\tKarate Kid\t9.5\n"
+                             "\tAvengers: Endgame\t15\n"
+                             "\tSnow White\t1.5\n"
+                             "Amount owed is 26\n"
+                             "You earned 4 frequent renter points");
+    }
+
 }
